77fdbd9403253fbf5ca1c98564b0d5dc168aa45a
Eamonn Keogh <7976992-eamonn-keogh@users.noreply.gitlab.com>
2023-08-20 00:00:51 +0100

    feat: include minus function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const add = (x: number, y: number) => {
+export const add = (x: number, y: number): number => {
     return x + y
+}
+
+export const minus = (x: number, y: number): number => {
+    return x - y;
 }
